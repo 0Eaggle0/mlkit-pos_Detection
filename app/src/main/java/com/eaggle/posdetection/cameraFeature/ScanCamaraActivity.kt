6e9b5607ba89efe3c2be63f53d7bc133c114be48
0Eaggle0 <rogoza.vladik@yandex.ru>
2023-04-23 15:31:35 +0600

    dfgh
    dfghjk

--- app/src/main/java/com/eaggle/posdetection/cameraFeature/ScanCamaraActivity.kt
+++ app/src/main/java/com/eaggle/posdetection/cameraFeature/ScanCamaraActivity.kt
@@ -1,13 +1,25 @@
 package com.eaggle.posdetection.cameraFeature
 
-import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.eaggle.posdetection.R
+import com.google.common.util.concurrent.ListenableFuture
 
-class ScanCamaraActivity: Activity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+class ScanCamaraActivity: AppCompatActivity() {
+    private lateinit var cameraFuture: ListenableFuture<ProcessCameraProvider>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activivty_camera)
+
+        cameraFuture = ProcessCameraProvider.getInstance(this)
+        cameraFuture.addListener({
+
+
+
+        }, ContextCompat.getMainExecutor(this))
     }
 }
--- app/src/main/java/com/eaggle/posdetection/startScan/StartFragment.kt
+++ app/src/main/java/com/eaggle/posdetection/startScan/StartFragment.kt
@@ -1,14 +1,33 @@
 package com.eaggle.posdetection.startScan
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.eaggle.posdetection.R
+import com.eaggle.posdetection.cameraFeature.ScanCamaraActivity
 
 class StartFragment: Fragment() {
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    )   { isGranted:Boolean ->
+            if (isGranted) {
+                startScan()
+            } else {
+                // homework: сделать так чтобы при отказе скрыть кнопку и показать сообщение
+            }
+        }
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,8 +39,20 @@ class StartFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val startButton = view.findViewById<Button>(R.id.scan_button)
+//        startButton.isVisible (homework)
         startButton.setOnClickListener {
-
+            val permission =  ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA)
+            when (permission) {
+                PackageManager.PERMISSION_GRANTED -> startScan()
+                else -> permissionLauncher.launch(Manifest.permission.CAMERA)
+            }
         }
+    }
+
+    private fun startScan(){
+        val saveIntent = Intent(requireContext(), ScanCamaraActivity::class.java)
+            startActivity(saveIntent)
     }
 }
